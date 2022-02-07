d34192227e811ec1e8ecc2a9dfd62f676d2a25f2
gdjohn4s <john4s@protonmail.ch>
2022-02-07 04:00:30 +0100

    Added GetChatID() in sendEndpoint.py - Now chatID is dinamically gained Added Comments in sendEndoint.py Removed sensitive log in blackKaliLoader script

--- src/sendEndpoint.py
+++ src/sendEndpoint.py
@@ -18,13 +18,17 @@
 import requests
 import re
 import sys
+from urllib3 import exceptions as exp
 
 REG = r'tcp://[^\"]*'
 TGTOKEN = ''
 CHATID = ''
 
 
-def telegram_bot_sendtext(bot_message): 
+def telegram_bot_sendtext(bot_message):
+    """
+    Send Telegram message with ngrok endpoint using Telegram Api
+    """
     try:
         send_text = f'https://api.telegram.org/bot{TGTOKEN}/sendMessage?chat_id={CHATID}&parse_mode=Markdown&text={bot_message}'
         response = requests.get(send_text)
@@ -33,16 +37,42 @@ def telegram_bot_sendtext(bot_message):
         print(f'[-] Connection aborted to Telegram API. Error: {conerr}')
 
 
+def getChatId():
+    """
+    Trying to get ChatID using Telegram API
+    """
+    updates = f'https://api.telegram.org/bot{TGTOKEN}/getUpdates'
+    req = requests.get(updates)
+    res = req.json()
+    if not res['result']:
+        print('[+] Can\'t get ChatID, insert manually!')
+        return None
+    else:
+        print('[+] ChatId gained')
+        return res['result'][0]['my_chat_member']['chat']['id']
+
+
 ################# MAIN EXECUTION #################
-if not TGTOKEN or not CHATID:
-    print("[-] Please insert a valid Telegram Bot Token or ChatID and rerun the program!")
+# If TelegramToken not set, it will exit
+if not TGTOKEN:
+    print("[-] Please insert a valid Telegram Bot Token and rerun the program!")
     sys.exit(1)
 else:
-    print(f'[+] Telegram Info found! -> {TGTOKEN}')
+    print('[+] Telegram Info found!')
+
+# If ChatID not set, it will try to get using telegram api by setting your telegram bot token
+if not CHATID:
+    print('[-] Hey don\'t you set ChatID in this code? Ok.. I\'ll Trying to get it from API..')
+    CHATID = getChatId()
+else:
+    print('[-] Error getting chatID from Telegram Api! Check your configuration')
+    sys.exit(1)
 
 data = ''
 respstat = ''
 
+# Here we got ngserver information by sending a get request to our ngrok session using ngrok api
+# If not reachable trow general exception
 try:
     ngresp = requests.get('http://127.0.0.1:4040/api/tunnels')
     respstat = ngresp.status_code
@@ -50,12 +80,15 @@ try:
 except Exception as conerr:
     print(f'[-] Connection aborted to Ngrok API. Error: {conerr}')
 
+# Using a Regex we'll get the exact endpoint value from ngrok api response
 endpoint = re.findall(REG, data)
 if len(endpoint) == 0:
     message = f'Failed to retrieve your endpoint, check ngrok configuration.\nthanks for using BlackKaliLoader!'
 else:
     message = f"Your Endpoint is {endpoint} , thanks for using BlackKaliLoader!"
 
+# Then finally send telegram message!
+# If 'ok' value from telegram json response is True then message sent otherwise False
 tgresp = telegram_bot_sendtext(str(message))
 if tgresp['ok'] is True:
     print("[+] Telegram message sent!")
